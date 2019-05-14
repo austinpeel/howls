06756ad4ab3587a5b42920c8edfb3a066c90be40
Austin <austin.peel@cea.fr>
2019-05-14 14:49:40 +0200

    Update to include step 2 maps

--- io.py
+++ io.py
@@ -24,20 +24,42 @@ def datapath(subdir=None):
     return path
 
 
-def kappapath(map_id, noisy=False, warnings=True):
+def kappapath(step, Om, map_id, noisy, warnings=True):
     """Retrieve path to HOWLS convergence map.
 
     Parameters
     ----------
+    step : int
+        Step number of the HOWLS project. Options so far are only {1, 2}.
+    Om : float or None, optional
+        Omega_m density parameter of the simulation (used in Step 2).
+        If None and step is 2, use the fiducial model.
     map_id : int
         Map realization number.
     noisy : bool
         Get the noisy version of the map if True.
 
     """
-    option = ['kappa', 'kappa_noise'][noisy]
-    filename = option + '_LCDM_zs2_step1_{:03d}.fits'.format(map_id)
-    filepath = os.path.join(datapath(), option, filename)
+    # Build data path to kappa maps
+    if step == 1:
+        option = ['kappa', 'kappa_noise'][noisy]
+        filename = option + '_LCDM_zs2_step1_{:03d}.fits'.format(map_id)
+        filepath = os.path.join(datapath('step1'), option, filename)
+    elif step == 2:
+        filename = 'kappa_noise_LCDM_'
+        if Om == None:
+            dir = 'LCDM_fiducial'
+            opt = ''
+        else:
+            dir = 'LCDM_Om_{}'.format(Om)
+            opt = 'Om_{}_'.format(Om)
+        filename += opt + 'zs2_step2_{:03d}.fits'.format(map_id)
+        filepath = os.path.join(datapath('step2'), dir, filename)
+    else:
+        if warnings:
+            print("Warning: invalid step number. Using 2.")
+            step = 2
+        return kappapath(step=2, Om=Om, map_id=map_id, noisy=noisy)
 
     if warnings:
         if not os.path.exists(filepath):
@@ -45,22 +67,27 @@ def kappapath(map_id, noisy=False, warnings=True):
     return filepath
 
 
-def fetch_kappa(map_id, noisy=False, subsize=None):
+def fetch_kappa(step, Om=None, map_id=0, noisy=False, subsize=None):
     """Retrieve a HOWLS convergence map.
 
     Parameters
     ----------
+    step : int
+        Step number of the HOWLS project. Options so far are only {1, 2}.
+    Om : float or None, optional
+        Omega_m density parameter of the simulation (used in Step 2).
+        If None and step is 2, use the fiducial model.
     map_id : int
         Map realization number.
     noisy : bool
-        Get the noisy version of the map if True.
+        Get the noisy version of the map if True (used in Step 1).
     subsize : int, optional
         If provided, return a random (subsize, subsize) section of the map.
 
     """
     kappa = None
     try:
-        kappa = fits.getdata(kappapath(map_id, noisy))
+        kappa = fits.getdata(kappapath(step, Om, map_id, noisy))
         if subsize is not None:
             subsize = int(subsize)
             x1 = np.random.randint(0, __NPIX__ - subsize)
